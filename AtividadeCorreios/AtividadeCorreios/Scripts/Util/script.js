0399fa3d72bbf20323ba7a3db62771c0300cf14d
Fabio de Oliveira <faabio.oliver@outlook.com>
2019-10-23 15:40:46 -0300

    Commit fim do javascript (quase morri)

--- AtividadeCorreios/AtividadeCorreios/Scripts/Util/script.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Util/script.js
@@ -1,5 +1,8 @@
 ﻿$(document).ready(function () {
+    //PEGAR A INFORMAÇÃO DO BOTÃO E ENVIAR OS DADOS PARA A URL
     $('input[name="consultar"]').click(function () {
+        //VARIAVEIS QUE VAMOS PEGAR DO INDEX
+
         var codigo = $('input[name="codigo"]').val();
         var senha = $('input[name="senha"]').val();
         var servicos = $('input[name="servicos"]').val();
@@ -13,16 +16,11 @@
         var valordeclarado = $('input[name="valordeclarado"]').val();
         var avisorecebimento = $('input[name="avisorecebimento"]').val();
 
-        $.get("http://usysweb.com.br/api/correiosambev.php?" + codigo + senha + servicos + ceporigem +
-            cepdestino + peso + comprimento + altura + largura + maopropria + valordeclarado + avisorecebimento, function (data) {
+        var url2 = "http://usysweb.com.br/api/correiosambev.php?" + "nCdEmpresa=" + codigo + "&sDsSenha=" + senha + "&sCepOrigem=" + ceporigem + "&sCepDestino=" + cepdestino + "&nVlPeso=" + peso + "&nCdFormato=0" + "&nVlComprimento=" + comprimento + "&nVlAltura=" + altura + "&nVlLargura=" + largura + "&sCdMaoPropria=" + maopropria + "&nVlValorDeclarado=0" + valordeclarado + "&sCdAvisoRecebimento=" + avisorecebimento + "&nCdServico=04510" + "&nVlDiametro=0&" + "StrRetorno=xml&nIndicaCalculo="
+        $.getJSON(url2, function (data) {
+            //alert("Valor: " + JSON.parse(data).cServico.Valor);
+            $('input[name="resultado"]').val(data.cServico.Valor);
 
-            }
-
-            $('input')
-
-
-        )
+        });
     });
-
-
 });
