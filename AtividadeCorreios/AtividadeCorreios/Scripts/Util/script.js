c196ac0ee7b5403a85380c41c62343bf492790cd
Fabio de Oliveira <faabio.oliver@outlook.com>
2019-10-23 17:26:10 -0300

    Commit TETETETETE

--- AtividadeCorreios/AtividadeCorreios/Scripts/Util/script.js
+++ AtividadeCorreios/AtividadeCorreios/Scripts/Util/script.js
@@ -19,7 +19,9 @@
         var url2 = "http://usysweb.com.br/api/correiosambev.php?" + "nCdEmpresa=" + codigo + "&sDsSenha=" + senha + "&sCepOrigem=" + ceporigem + "&sCepDestino=" + cepdestino + "&nVlPeso=" + peso + "&nCdFormato=0" + "&nVlComprimento=" + comprimento + "&nVlAltura=" + altura + "&nVlLargura=" + largura + "&sCdMaoPropria=" + maopropria + "&nVlValorDeclarado=0" + valordeclarado + "&sCdAvisoRecebimento=" + avisorecebimento + "&nCdServico=04510" + "&nVlDiametro=0&" + "StrRetorno=xml&nIndicaCalculo="
         $.getJSON(url2, function (data) {
             //alert("Valor: " + JSON.parse(data).cServico.Valor);
-            $('input[name="resultado"]').val(data.cServico.Valor);
+            $('input[name="resultadoValor"]').val(data.cServico.Valor);
+            $('input[name="resultadoPrazo"]').val(data.cServico.PrazoEntrega + " dias");
+            $('input[name="resultadoPrazo"]').val(data.cServico.PrazoEntrega + " dias");
 
         });
     });
